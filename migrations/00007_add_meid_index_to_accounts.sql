e7ccf8bff6f08835abd7a2d8128e4c87523b8a86
gw123 <963353840@qq.com>
2020-03-18 20:18:16 +0800

    remove file

--- migrations/00007_add_meid_index_to_accounts.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-ALTER TABLE `echoapp`.`accounts` ADD INDEX `idx_accounts_meid` (`meid`);
-
--- +goose Down
-ALTER TABLE `echoapp`.`accounts` DROP INDEX `idx_accounts_meid`;
--- migrations/00008_add_link_column_to_upgrades.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE `echoapp`.`upgrades`
-ADD COLUMN `link` varchar(1024) DEFAULT '' COMMENT '文件公共下载链接';
-
--- +goose Down
-ALTER TABLE `echoapp`.`upgrades`
-DROP COLUMN `link`;
